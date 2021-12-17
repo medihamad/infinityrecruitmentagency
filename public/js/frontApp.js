818caf44b2854d858c74539ff35867887a6f463e
medihamad <kumarpatel692@gmail.com>
2021-12-17 09:21:29 +0300

    all new files

---
+++ public/js/frontApp.js
@@ -0,0 +1,67 @@
+//selecting the elements for the first form passport photo
+
+const prepend_small_btn1 = document.querySelector('#prepend-small-btn1');
+const passport_photo1 = document.querySelector('#passport_photo1');
+const file_upload1 = document.querySelector('#file_upload1');
+const candidate_name1 = document.querySelector('#candidate_name1');
+const student_inner_std = document.querySelector('.student-inner-std');
+
+passport_photo1.style.display = 'none';
+
+//selecting the elements for the second form full photo
+
+const prepend_small_btn2 = document.querySelector('#prepend-small-btn2');
+const passport_photo = document.querySelector('#passport_photo');
+const file_upload = document.querySelector('#file_upload');
+const candidate_name = document.querySelector('#candidate_name');
+
+passport_photo.style.display = 'none';
+
+file_upload1.addEventListener('change', (e)=>{
+      // displaying the image after selecting from the file
+      passport_photo1.style.display = 'block';
+
+      //displaying passport photo on the page after selecting from file
+       passport_photo1.src = 'img/'+ file_upload1.files[0].name;
+   
+       //showing the path to the file in the text box
+       prepend_small_btn1.value = file_upload1.files[0].name;
+   
+       //displaying the name of the file under the passport photo (as the name of the file is the name of the candidate)
+       candidate_name1.innerText = file_upload1.files[0].name;
+   
+   })
+
+file_upload.addEventListener('change', (e)=>{
+     // displaying the image after selecting from the file
+     passport_photo.style.display = 'block';
+   
+   //displaying passport photo on the page after selecting from file
+    passport_photo.src = 'img/'+ file_upload.files[0].name;
+
+    //showing the path to the file in the text box
+    prepend_small_btn2.value = file_upload.files[0].name;
+
+    //displaying the name of the file under the passport photo (as the name of the file is the name of the candidate)
+    candidate_name.innerText = file_upload.files[0].name;
+    const img_hidden = student_inner_std.children[0].children[0].style.opacity = '1'
+
+});
+
+{/* <div class="chosen-select-single mg-b-20 form-group">
+                                                                   
+<select data-placeholder="Choose a Country..." class="chosen-select form-control" tabindex="-1">
+        <option value="">Select Gender</option>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+</select>
+</div>
+
+<div class="chosen-select-single mg-b-20">
+
+<select data-placeholder="Choose a Country..." class="chosen-select form-control" tabindex="-1">
+        <option value="">Select Job category</option>
+        <option value="House maid">House maid</option>
+        <option value="Nurse">Nurse</option>
+</select>
+</div> */}
---
+++ tmp/js/myjavascript.js
@@ -0,0 +1,30 @@
+﻿function views() {
+    var element1 = document.getElementById("viewz");
+    element1.style.borderBottom = "4px solid #6c757d";
+}
+
+function news() {
+    var element2 = document.getElementById("newz");
+    element2.style.borderBottom = "4px solid #6c757d";
+
+    var element3 = document.getElementById("viewz");
+    element3.style.border = "none";
+}
+
+function views1() {
+    var element4 = document.getElementById("viewz");
+    element4.style.borderBottom = "4px solid #6c757d";
+
+    var element5 = document.getElementById("newz");
+    element5.style.border = "none";
+}
+
+function hidenew() {
+    var element4 = document.getElementById("new-persone");
+    element4.style.display = "none";
+}
+
+function shownew() {
+    var element4 = document.getElementById("new-persone");
+    element4.style.display = "block";
+}
